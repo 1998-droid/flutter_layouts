63f7276b5dbf5373f23a80c7c2a5424a9902fbab
1998-droid <tiotiotio98@gmail.com>
2022-06-24 11:36:05 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,31 +6,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'First App',
-      home: Scaffold(
-          backgroundColor: Color.fromARGB(255, 158, 224, 15),
-          appBar: AppBar(
-            title: Text('kontol'),
-            backgroundColor: Colors.orange,
-          ),
-          body: Column(children: <Widget>[
-            Image.network(
-                'https://static.republika.co.id/uploads/images/inpicture_slide/nft-the-bored_211214191049-538.jpg'),
-            Text(
-              'Belajar Flutter untuk Pemula',
-              style: TextStyle(fontSize: 24, fontFamily: "Serif", height: 2.0),
+        title: 'First App',
+        home: Scaffold(
+            backgroundColor: Color.fromARGB(255, 158, 224, 15),
+            appBar: AppBar(
+              title: Text('kontol'),
+              backgroundColor: Colors.orange,
             ),
-            Text('oleh Petani Kode'),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Image.network(
-                    'https://goalkes-images.s3-ap-southeast-1.amazonaws.com/hospitals/e7MnaDDWgHpFoWDAYvAlyHsVlDaOJ1bC2UgtceWF.jpg'),
-                Image.network(
-                    'https://www.garnesia.com/images/vendor/5708_82f5e8d705b46b0dabc70085a7f779f7.jpg')
-              ],
-            )
-          ])),
-    );
+            body: Column(children: <Widget>[
+              Image.asset('assets/pic/vaksin.jpg'), // tanpa BlendMode
+              Image.asset('assets/pic/vaksin.jpg', // dengan BlendMode
+                  color: Colors.grey,
+                  colorBlendMode: BlendMode.hue),
+            ])));
   }
 }
